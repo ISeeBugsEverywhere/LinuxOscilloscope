2ad2f9c6f44b04f90c5792b194e267d3dc8c5971
userIsTaken <user@user.com>
2019-04-16 17:08:54 +0300

    Initial commit

--- HWaccess/RS232Device.py
+++ HWaccess/RS232Device.py
@@ -5,17 +5,20 @@ from PyQt5 import QtSerialPort
 
 
 class RS232Device(QObject, QtSerialPort):
-    def __init__(self, port:QtSerialPort):
+    def __init__(self, port:QtSerialPort.QSerialPort()):
         super(RS232Device, self).__init__()
-        self._port = port
+        self._port = QtSerialPort.QSerialPort()
+        # self._port = port
         self._port.open(QIODevice.ReadWrite)
         self._output = None
         self._terminator= None
         self._encoding = None
         self._error_behavior = None
+        self._line_end = None
         pass
 
     def write(self, cmd:str):
+        self._port.write(bytes((cmd)+self._line_end, self._encoding))
         pass
 
     def read(self):
