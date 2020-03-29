84b56f0235e97f29e84363e50ac9bd824de309c7
bugsStillEverywhere <menulio.svytuokle@gmail.com>
2020-03-29 21:10:10 +0300

    restruct

--- HWaccess/Device.py
+++ HWaccess/Device.py
@@ -1,5 +1,5 @@
-from vxi11 import vxi11
 from HWaccess.USBTMC import USBTMC
+from HWaccess.LXI import lxi
 
 
 class Device:
@@ -45,43 +45,4 @@ class Device:
         return answer, status
 
 
-class lxi:
-    def __init__(self, ip: str):
-        self.device = vxi11.Instrument(ip)
-        self.device.open()
-        pass
 
-    def ask(self, cmd:str):
-        """
-
-        :param cmd: cmd in string
-        :return: answer, 0 if OK, else exception, -1
-        """
-        try:
-            answer = self.device.ask(cmd)
-            return answer, 0
-        except Exception as ex:
-            return str(ex), -1
-        pass
-
-    def write(self, cmd:str):
-        """
-
-        :param cmd: cmd string
-        :return: None, 0, if ok, else - None, -1
-        """
-        try:
-            self.device.write(cmd)
-            return None, 0
-        except Exception as ex:
-            return str(ex), -1
-        pass
-
-    def getName(self):
-        name = self.device.ask("*idn?")
-        return name
-        pass
-
-    def sendReset(self):
-        self.device.write("*rst")
-        pass
---
+++ HWaccess/LXI.py
@@ -0,0 +1,43 @@
+from vxi11 import vxi11
+
+
+class lxi:
+    def __init__(self, ip: str):
+        self.device = vxi11.Instrument(ip)
+        self.device.open()
+        pass
+
+    def ask(self, cmd:str):
+        """
+
+        :param cmd: cmd in string
+        :return: answer, 0 if OK, else exception, -1
+        """
+        try:
+            answer = self.device.ask(cmd)
+            return answer, 0
+        except Exception as ex:
+            return str(ex), -1
+        pass
+
+    def write(self, cmd:str):
+        """
+
+        :param cmd: cmd string
+        :return: None, 0, if ok, else - None, -1
+        """
+        try:
+            self.device.write(cmd)
+            return None, 0
+        except Exception as ex:
+            return str(ex), -1
+        pass
+
+    def getName(self):
+        name = self.device.ask("*idn?")
+        return name
+        pass
+
+    def sendReset(self):
+        self.device.write("*rst")
+        pass
