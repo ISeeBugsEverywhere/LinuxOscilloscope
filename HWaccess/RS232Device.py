af92b635f4a499f54c08d5d033abbc62cb1dcf09
bugsStillEverywhere <menulio.svytuokle@gmail.com>
2020-03-29 21:31:08 +0300

    restruct

--- HWaccess/RS232Device.py
+++ HWaccess/RS232Device.py
@@ -1,7 +1,16 @@
 import os
 import time
+import serial
 
 
 class RS232Device():
     def __init__(self, port):
+        self.serial = serial.Serial()
+        self.serial.setPort(port)
+        pass
+
+    def ask(self, cmd:str):
+        pass
+
+    def write(self, cmd:str):
         pass
