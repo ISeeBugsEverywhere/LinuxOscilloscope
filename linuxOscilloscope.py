68f3123f351a354d719efcf8f8810bf751b8487a
ISeeBugsEverywhere <menulio.svytuokle@gmail.com>
2020-06-03 17:41:14 +0300

    dlg

--- linuxOscilloscope.py
+++ linuxOscilloscope.py
@@ -50,10 +50,10 @@ class LOsc(QtWidgets.QMainWindow):
         self.ui.rs232Widget.ui.comPortBox.currentIndexChanged.connect(self.idxfn)
         # buttons:
         self.ui.connectButton.clicked.connect(self.connect_device_fn)
-        self.ui.ch1_btn.clicked.connect(self.checked_fn1)
-        self.ui.ch2_btn.clicked.connect(self.checked_fn2)
-        self.ui.ch3_btn.clicked.connect(self.checked_fn3)
-        self.ui.ch4_btn.clicked.connect(self.checked_fn4)
+        # self.ui.ch1_btn.clicked.connect(self.checked_fn1)
+        # self.ui.ch2_btn.clicked.connect(self.checked_fn2)
+        # self.ui.ch3_btn.clicked.connect(self.checked_fn3)
+        # self.ui.ch4_btn.clicked.connect(self.checked_fn4)
         self.ui.get_data_btn.clicked.connect(self.get_data_fn)
         self.ui.take_screenshot_btn.clicked.connect(self.screenshot_fn)
         pass
@@ -88,68 +88,9 @@ class LOsc(QtWidgets.QMainWindow):
                 pass
         pass
 
+    def check_channel_btn(self):
 
-
-
-    def checked_fn1(self):
-        try:
-            if not self.ui.ch1_btn.isChecked():
-                ch = self._channels[1]
-                self._active_channels.remove(ch)
-                pass
-                # print('ch1')
-                # self._channels.remove('ch1')
-            elif self.ui.ch1_btn.isChecked():
-                ch = self._channels[1]
-                self._active_channels.append(ch)
-                # self._channels.append('ch1')
-                # print('ch1 ...')
-                pass
-        except Exception as ex:
-            print(str(ex))
-            pass
-
-    def checked_fn2(self):
-        try:
-            if not self.ui.ch2_btn.isChecked():
-                ch = self._channels[2]
-                self._active_channels.remove(ch)
-                print('ch2')
-            elif self.ui.ch2_btn.isChecked():
-                ch = self._channels[2]
-                self._active_channels.append(ch)
-                print('ch2 ...')
-        except Exception as ex:
-            print(str(ex))
-            pass
-
-    def checked_fn3(self):
-        try:
-            if not self.ui.ch3_btn.isChecked():
-                ch = self._channels[3]
-                self._active_channels.remove(ch)
-                print('ch3')
-            elif self.ui.ch3_btn.isChecked():
-                ch = self._channels[3]
-                self._active_channels.append(ch)
-                print('ch3 ...')
-        except Exception as ex:
-            print(str(ex))
-            pass
-
-    def checked_fn4(self):
-        try:
-            if not self.ui.ch4_btn.isChecked():
-                ch = self._channels[4]
-                self._active_channels.remove(ch)
-                print('ch4')
-            elif self.ui.ch4_btn.isChecked():
-                ch = self._channels[4]
-                self._active_channels.append(ch)
-                print('ch4 ...')
-        except Exception as ex:
-            print(str(ex))
-            pass
+        pass
 
     def setup_gui_fn(self):
         self.update_ports_fn()
