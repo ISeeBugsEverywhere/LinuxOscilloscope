b5882eb17d9649271c54d7cfd815c6ea57a35608
bugsStillEverywhere <menulio.svytuokle@gmail.com>
2020-06-18 00:23:37 +0300

    dlg

--- HWaccess/USBTMC_mod.py
+++ HWaccess/USBTMC_mod.py
@@ -1,3 +1,16 @@
 import usbtmc
 class USBTMC:
-    pass
+    def __init__(self, device):
+        self.device = usbtmc.Instrument(device)
+        pass
+
+    @staticmethod
+    def get_devices(self):
+        """
+        returns alist with usbtmc devices
+        (Windows only version)
+
+        :return:
+        """
+        return usbtmc.list_devices()
+
