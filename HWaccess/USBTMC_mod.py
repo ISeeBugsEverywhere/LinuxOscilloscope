4db63669288d122d0b48e9eadd7e070b6126ac80
bugsStillEverywhere <menulio.svytuokle@gmail.com>
2020-06-18 07:35:21 +0300

    dlg

--- HWaccess/USBTMC_mod.py
+++ HWaccess/USBTMC_mod.py
@@ -14,3 +14,31 @@ class USBTMC:
         """
         return usbtmc.list_devices()
 
+    def write(self, command):
+        try:
+            self.device.write(command)
+            return None, 0
+        except Exception as ex:
+            return str(ex), -1
+            pass
+
+    def read(self, length=4000):
+        '''
+
+        :param length: length of data to read
+        :return:
+        '''
+        return self.device.read_raw(length)
+
+    def close(self):
+        self.device.close()
+
+    def ask(self, cmd, sleep=0, length=4000):
+        """
+        Imitates vxi11 ask command, therefore returns a string
+        :param cmd:
+        :return:
+        """
+        a = self.device.ask_raw(cmd, length)
+        return str(a.decode('utf-8'))
+
