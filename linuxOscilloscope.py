9deeff3bb50b67144e7fd582c152c721b1185f9d
ISeeBugsEverywhere <menulio.svytuokle@gmail.com>
2021-11-18 11:40:27 +0200

    various updates

--- linuxOscilloscope.py
+++ linuxOscilloscope.py
@@ -240,7 +240,7 @@ class LOsc(QtWidgets.QMainWindow):
         _lo = len(_o)
         large_txt = ""
         for i in _o:
-            large_txt = large_txt + str(i)+self._new_line
+            large_txt = large_txt + str(i)+"\n"
         self.append_html_paragraph(large_txt, 0, False)
         pass
 
